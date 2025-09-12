3473957eeed36bc8b6c2d8697959b80d8dbc41e4
dr-ben-onuorah <osonduonuorah@gmail.com>
2025-09-12 14:44:19 +0100

    work on Program.cs

--- OnuInstitute/Program.cs
+++ OnuInstitute/Program.cs
@@ -63,18 +63,12 @@ builder.Services.AddAuthorization(Options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 //if (app.Environment.IsDevelopment())
 //{
     app.UseSwagger();
     app.UseSwaggerUI();
 //}
-
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/ swagger / v1 / swagger.json", "v1");
-    c.RoutePrefix = "swagger"; // ensures UI is at /swagger
-});
 
 
 app.UseStaticFiles();
